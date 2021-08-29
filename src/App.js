ac331b75406aafcefcced19faf458e1205003d76
Mark Otweyo <markotweyo99@gmail.com>
2021-08-29 06:33:49 +0300

    Implementing Rating Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 
 
@@ -12,6 +13,7 @@ const App= () => {
     <main className='py-3'>
       <Container>
       <h1>Welcome to ProShop</h1>
+      <HomeScreen/>
       </Container>
     </main>
     <Footer/>       
